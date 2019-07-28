66ee76ae401c2a29087c77b60b7a755d847f2167
ZProgrammer77 <Zkhodadadi77@yahoo.com>
2019-07-28 19:37:42 +0430

    login

---
+++ Rejime/Scripts/Operation.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    $('#Register').bootstrapValidator({
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+
+    });
+});
